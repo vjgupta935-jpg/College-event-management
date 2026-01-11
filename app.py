2eaae80118d2163a33dba179f134a453b9b8a402
VIJAY GUPTA <vjgupta935@gmail.com>
2026-01-11 21:13:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -557,4 +557,6 @@ def init_database():
     print("🎉 All features working: Events, Registration, Admin Panel, Login Tracking")
     print()
 
-    app.run(debug=True, host='0.0.0.0', port=5000)"""
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+"""
